1f40a8026224474d4fa12aebf19ac17d06c5e024
MaryniakT <114291060+MaryniakT@users.noreply.github.com>
2023-09-30 10:49:09 +0300

    hw3

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,18 +1,14 @@
-const {HttpError} = require('../helpers')
+const { HttpError } = require("../helpers");
 
-const validateBody = schema => {
+const validateBody = (schema) => {
   const func = (req, res, next) => {
-      const body = req.body;
-    if (Object.keys(body).length === 0) {
-      throw HttpError(400, 'missing fields');
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(HttpError(400, error.message));
     }
-        const { error } = schema.validate(req.body);
-         if (error) {
-           next(HttpError(400, error.message));
-         }
-     next();
-   }
-   return func;
-}
+    next();
+  };
+  return func;
+};
 
-module.exports = {validateBody}
+module.exports = validateBody;
