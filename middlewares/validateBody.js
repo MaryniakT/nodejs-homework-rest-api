27160cad7eb40060716e943d663051b031de07dc
MaryniakT <114291060+MaryniakT@users.noreply.github.com>
2023-10-06 18:28:45 +0300

    validateBody

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,16 +4,12 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
  const body = req.body;
     if (Object.keys(body).length === 0) {
-      throw HttpError(400, 'missing fields');
+      throw HttpError(400, 'missing field favorite');
     }
     
-     if (req.body.favorite === undefined) {
-      return res.status(400).json({ message: "Missing field favorite" });
-    }
-
-    if (typeof req.body.favorite !== "boolean") {
-      return res.status(400).json({ message: "Invalid type for favorite" });
-    }
+    // if (typeof req.body.favorite !== "boolean") {
+    //   return res.status(400).json({ message: "Invalid type for favorite" });
+    // }
 
 
     const { error } = schema.validate(req.body);
